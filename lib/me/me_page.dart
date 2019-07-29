4f5bd73f6bb4c13901deb9ac0ee718cc9fdda7c4
yichuancq <1012027293@qq.com>
2019-07-29 21:50:42 +0800

    add 添加个人中心布局

--- lib/me/me_page.dart
+++ lib/me/me_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_highspeed_railway/widget/common_widget.dart';
 
+/// me page
 class MePage extends StatefulWidget {
   MePage({Key key}) : super(key: key);
 
@@ -8,22 +10,146 @@ class MePage extends StatefulWidget {
   _MePageState createState() => _MePageState();
 }
 
+/// me
 class _MePageState extends State<MePage> {
+  ///header
+  Widget _header() {
+    return Container(
+      height: 130,
+      width: MediaQuery.of(context).size.width,
+//      color: Colors.yellow,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ClipOval(
+              child: Image.asset(
+            "assets/image/girl2.jpeg",
+            height: 100.0,
+          )),
+          SizedBox(
+            width: 5.0,
+          ),
+          Text(
+            "yichuan",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+          ),
+        ],
+      ),
+    );
+  }
+
+  ///全部订单，待支付，待出行
+  Widget _headerBarBtn() {
+    /// 全部订单，待支付，待出行
+    var list = <Widget>[];
+    var map = {
+      "全部订单": Icons.event_note,
+      "待支付": Icons.payment,
+      "待出行": Icons.card_travel,
+    };
+    map.forEach((key, value) {
+      if (key.startsWith("待支付")) {
+        list.add(SizedBox(
+          width: 60,
+          child: MyImageButton(
+            image: Icon(value, size: 30.0),
+            title: key,
+            tip: ("两笔"),
+          ),
+        ));
+      } else {
+        list.add(SizedBox(
+          width: 60,
+          child: MyImageButton(image: Icon(value, size: 30.0), title: key),
+        ));
+      }
+    });
+    return Container(
+      margin: EdgeInsets.only(left: 50, right: 50),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: list,
+      ),
+    );
+  }
+
+  Widget _myToolMenu(Widget image, String title, String subTitle) {
+    return Container(
+      padding: EdgeInsets.only(left: 10, right: 10, top: 20, bottom: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.only(left: 10, right: 10, top: 0, bottom: 0),
+              child: Row(
+                children: <Widget>[
+                  image,
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Text(
+                    title,
+                    style: TextStyle(fontSize: 12.0),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Text(
+            subTitle,
+            style: TextStyle(fontSize: 10.0, color: Colors.grey),
+          ),
+          Icon(Icons.chevron_right),
+        ],
+      ),
+    );
+  }
+
+  ///nave_menu
+  Widget _builderToolMenu() {
+    /// 我的卡券，用户反馈，客服热线，关于软件
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+      child: Column(
+        children: <Widget>[
+          _myToolMenu(new Icon(Icons.card_membership), "我的卡券", "8张"),
+          _myToolMenu(new Icon(Icons.message), "用户反馈", ""),
+          _myToolMenu(new Icon(Icons.verified_user), "客服热线", "8:30-21:00"),
+          _myToolMenu(new Icon(Icons.info), "关于软件", "v2.0.5"),
+        ],
+        //children: list,
+      ),
+    );
+  }
+
+  ///
+  Widget _builderBody() {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          _header(),
+          _headerBarBtn(),
+          _builderToolMenu(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'MePage',
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarOpacity: 1,
+        actions: <Widget>[
+          IconButton(
+            icon: new Icon(Icons.notifications),
+          )
+        ],
       ),
+      body: _builderBody(),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
---
+++ lib/widget/common_widget.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+///自定义部件
+class MyImageButton extends StatelessWidget {
+  MyImageButton({@required this.image, @required this.title, this.tip});
+
+  final Widget image;
+  final String title;
+  final String tip;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+//      onTap: () => Navigator.of(context).push(CupertinoPageRoute(
+//          builder: (context) => TestPage(
+//            content: title,
+//          ))),
+      splashColor: Colors.grey,
+      child: Stack(
+        children: <Widget>[
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                SizedBox(
+                  height: 12,
+                ),
+                image,
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  title,
+                  style: TextStyle(fontSize: 12.0),
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: tip != null
+                ? MyTag(
+                    tag: tip,
+                    isEmphasize: true,
+                    radius: 15.0,
+                  )
+                : null,
+          ),
+        ],
+      ),
+    );
+  }
+}
+//
+//class PriceText extends StatelessWidget {
+//  PriceText(this._price);
+//
+//  final String _price;
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return RichText(
+//      text: TextSpan(children: <TextSpan>[
+//        TextSpan(
+//            text: "￥", style: TextStyle(fontSize: 10.0, color: Colors.red)),
+//        TextSpan(
+//            text: _price,
+//            style: TextStyle(
+//                fontSize: 15.0,
+//                color: Colors.red,
+//                fontWeight: FontWeight.bold)),
+//        TextSpan(
+//            text: " 起", style: TextStyle(fontSize: 10.0, color: Colors.black))
+//      ]),
+//    );
+//  }
+//}
+
+class MyTag extends StatelessWidget {
+  MyTag({@required this.tag, this.isEmphasize = false, this.radius = 3.0});
+
+  final String tag;
+  final bool isEmphasize;
+  final double radius;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+            color: isEmphasize ? Colors.red : Colors.black, width: 0.5),
+        color: isEmphasize ? Colors.red : Colors.white,
+        borderRadius: BorderRadius.circular((radius)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 3.0, right: 3.0, bottom: 1.0),
+        child: Text(
+          tag,
+          style: TextStyle(
+              fontSize: 10.0, color: isEmphasize ? Colors.white : Colors.black),
+        ),
+      ),
+    );
+  }
+}
+
+class TextTag extends StatelessWidget {
+  TextTag(
+      {@required this.tag,
+      this.padding = const EdgeInsets.all(5),
+      this.radius = 5,
+      this.onTap});
+
+  final String tag;
+  final EdgeInsets padding;
+  final double radius;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(radius),
+        child: Container(
+          margin: const EdgeInsets.all(5),
+          padding: padding,
+          //EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          color: Colors.grey[100],
+          child: Text(
+            tag,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildFakeSearchBox(IconData icon, String label) {
+  return Card(
+    elevation: 0.0,
+    shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(45.0))),
+    color: CupertinoColors.lightBackgroundGray,
+    child: Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Row(
+        children: <Widget>[
+          Icon(
+            icon,
+            color: Colors.grey,
+            size: 22,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            label,
+            style: TextStyle(fontSize: 16, color: Colors.grey, height: 0.8),
+          )
+        ],
+      ),
+    ),
+  );
+}
